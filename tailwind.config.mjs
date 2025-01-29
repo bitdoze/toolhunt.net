6106718121f07b15b24a85d2761ba0ef133a4050
Dragos B <bitdoze1@gmail.com>
2025-01-29 15:44:07 +0200

    hero added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,25 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 3s linear infinite',
+        'gradient-text': 'gradient-x 4s linear infinite',
+        'blob': 'blob 7s infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+        'blob': {
+          '0%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0px, 0px) scale(1)' },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
