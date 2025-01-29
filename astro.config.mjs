8f3f0d8791dc5f60934a8a9c91b0a1d3d84e8340
Dragos B <bitdoze1@gmail.com>
2025-01-29 16:06:47 +0200

    fixed some issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://directory.example.com',
+  site: 'https://toolhunt.net',
   integrations: [
     tailwind(),
     markdoc(),
