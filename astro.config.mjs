5cdcfecffd10ffa61d34a1cd3173c8b4ddd7a078
Dragos B <bitdoze1@gmail.com>
2025-01-31 14:20:36 +0200

    added filters

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import markdoc from '@astrojs/markdoc';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://toolhunt.net',
+  adapter: cloudflare(),
   integrations: [
     tailwind(),
     markdoc(),
